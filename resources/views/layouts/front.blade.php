d3c9dce288b6a638cc4da6c35b4b469464862e72
okssa <okssaamisantosa21@gmail.com>
2020-07-18 19:37:42 +0700

    login register

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -60,7 +60,34 @@
                 </div>
                 <div class="electronics-login-register">
                     <ul>
-                        <li><a href="#"><i class="pe-7s-users"></i>My Account</a></li>
+                                @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
@@ -81,14 +108,14 @@
                     </div>
                 </div>
                 <div class="trace-cart-wrapper">
-                    <div class="trace same-style">
+                    {{-- <div class="trace same-style">
                         <div class="same-style-icon">
                             <a href="#"><i class="pe-7s-plane"></i></a>
                         </div>
                         <div class="same-style-text">
                             <a href="#">Product <br>trace</a>
                         </div>
-                    </div>
+                    </div> --}}
                     <div class="categories-cart same-style">
                         <div class="same-style-icon">
                             <a href="#"><i class="pe-7s-cart"></i></a>
