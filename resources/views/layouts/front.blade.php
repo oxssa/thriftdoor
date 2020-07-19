e2836eb6d862cc4685dab8944ac3c10faa7a771a
okssa <okssaamisantosa21@gmail.com>
2020-07-19 15:28:01 +0700

    color slider

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="/assets/css/bundle.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/responsive.css">
+    <link rel="stylesheet" href="/assets/css/ralat.css">
     @livewireStyles
     <script src="/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
